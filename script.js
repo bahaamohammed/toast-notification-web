5d781883797e5ac059f70efd6fe829dbb25139ac
Bahaa Mohammed <45239532+bahaamohammed@users.noreply.github.com>
2024-10-13 18:24:35 +0300

    Complete the project

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 let toastBox = document.getElementById("toastBox");
-let successMsg = '<i class="fa-solid fa-circle-check"></i>Successfully submitted';
-let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error';
-let warnMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, check again'
+let successMsg = '<i class="fa fa-solid fa-circle-check"></i>Successfully submitted';
+let errorMsg = '<i class="fa fa-solid fa-circle-xmark"></i>Please fix the error';
+let warnMsg = '<i class="fa fa-solid fa-circle-exclamation"></i>Invalid input, check again'
 
-function showToast(){
+function showToast(button){
     let msg = '';
+    let toast = document.createElement('div');
     if(button.classList.contains('toast-success')){
         msg = successMsg;
+        toast.classList.add('toast');
+        toast.classList.add('success');
     } else if(button.classList.contains('toast-warn')){
         msg = warnMsg;
+        toast.classList.add('toast');
+        toast.classList.add('warning');
     }else{
         msg = errorMsg;
+        toast.classList.add('toast');
+        toast.classList.add('error');
     }
-    let toast = document.createElement('div');
-    toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
+    setTimeout(()=>{
+        toast.remove();
+    }, 6000);
 }

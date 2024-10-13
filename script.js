7f20e3c9148ebafa9a1a461343dda85eecf4143e
Bahaa Mohammed <45239532+bahaamohammed@users.noreply.github.com>
2024-10-13 18:31:05 +0300

    add icon to close toast

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 let toastBox = document.getElementById("toastBox");
 let successMsg = '<i class="fa fa-solid fa-circle-check"></i>Successfully submitted';
 let errorMsg = '<i class="fa fa-solid fa-circle-xmark"></i>Please fix the error';
-let warnMsg = '<i class="fa fa-solid fa-circle-exclamation"></i>Invalid input, check again'
+let warnMsg = '<i class="fa fa-solid fa-circle-exclamation"></i>Invalid input, check again';
 
-function showToast(button){
+function showToast(button) {
     let msg = '';
     let toast = document.createElement('div');
-    if(button.classList.contains('toast-success')){
+
+    // Determine the type of toast based on the button clicked
+    if (button.classList.contains('toast-success')) {
         msg = successMsg;
-        toast.classList.add('toast');
-        toast.classList.add('success');
-    } else if(button.classList.contains('toast-warn')){
+        toast.classList.add('toast', 'success');
+    } else if (button.classList.contains('toast-warn')) {
         msg = warnMsg;
-        toast.classList.add('toast');
-        toast.classList.add('warning');
-    }else{
+        toast.classList.add('toast', 'warning');
+    } else {
         msg = errorMsg;
-        toast.classList.add('toast');
-        toast.classList.add('error');
+        toast.classList.add('toast', 'error');
     }
-    toast.innerHTML = msg;
+
+    toast.innerHTML = `
+        ${msg}
+        <i class="fa fa-solid fa-circle-xmark close-toast"></i>
+    `;
+
+    // Add the toast to the DOM
     toastBox.appendChild(toast);
 
-    setTimeout(()=>{
+    // Add click event listener to the close icon
+    toast.querySelector('.close-toast').addEventListener('click', function() {
+        toast.remove();  // Remove the toast when the close icon is clicked
+    });
+
+    // Automatically fade out and remove the toast after 6 seconds
+    setTimeout(() => {
+        toast.classList.add('fade-out');
+    }, 5500);
+
+    setTimeout(() => {
         toast.remove();
     }, 6000);
 }
